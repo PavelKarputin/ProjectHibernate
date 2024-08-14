637d87a3efa4130fc4c7ab882f2f6c44b5e8c051
pachom <pachom96@inbox.ru>
2024-08-14 21:31:13 +0700

    Commit MySQL

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
